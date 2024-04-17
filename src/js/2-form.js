5b8f3beab90afbb6c45d46681b587868588b8ec2
OksanaGukova <kseniagukova@ukr.net>
2024-04-17 19:21:46 +0300

    Update 2-form.js

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -7,31 +7,35 @@ const messageTextarea = form.querySelector('textarea[name="message"]');
 
 form.addEventListener('input', () => {
   const formData = {
-    email: emailInput.value,
-    message: messageTextarea.value,
+    email: emailInput.value.trim(),
+    message: messageTextarea.value.trim(),
   };
   localStorage.setItem(localStorageKey, JSON.stringify(formData));
 });
 
 function isContentSaved() {
     const feedbackFormState = JSON.parse(localStorage.getItem(localStorageKey));
-  if (feedbackFormState) {
+  if (feedbackFormState!== null) {
     emailInput.value = feedbackFormState.email || '';
     messageTextarea.value = feedbackFormState.message || '';
     }
 }; 
 
-document.addEventListener('DOMContentSaved', isContentSaved);
+document.addEventListener('DOMContentLoaded', isContentSaved);
 
 form.addEventListener('submit', (event) => {
   event.preventDefault(); 
   const formData = {
-    email: emailInput.value,
-    message: messageTextarea.value,
+    email: emailInput.value.trim(),
+    message: messageTextarea.value.trim(),
   };
-  localStorage.removeItem(localStorageKey)
+ if (formData.email && formData.message) {
+    console.log(formData); 
+    localStorage.removeItem(localStorageKey);
     form.reset();
-     console.log(formData); 
+  } else {
+    alert("Please fill in both email and message fields.");
+  }
 }); 
 
 
